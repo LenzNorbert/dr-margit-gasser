7863f140f768ba7162811f6578f386461affd714
schmaenjael <mayer.manuel@protonmail.ch>
2024-01-26 16:36:33 +0100

    header

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -3,7 +3,18 @@ import { storyblokEditable } from '@storyblok/react';
 export const About = ({ blok }: any) => {
   return (
     <div {...storyblokEditable(blok)} key={blok?._uid} id={blok?._uid} className="relative h-screen w-full">
-      <h1>About</h1>
+      <div
+        style={{
+          background: 'rgba(255, 255, 255, 0.54)',
+          backdropFilter: 'blur(6.5px)',
+          WebkitBackdropFilter: 'blur(6.5px)',
+          borderRadius: '16px',
+          boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
+          border: '1px solid rgba(255, 255, 255, 0.87)',
+        }}
+        className="h-full w-10/12">
+        test
+      </div>
     </div>
   );
 };
--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -2,9 +2,16 @@ import { storyblokEditable } from '@storyblok/react';
 
 export const Header = ({ blok }: any) => {
   return (
-    <header {...storyblokEditable(blok)} key={blok?._uid} id={blok?._uid} className="relative h-screen w-full bg-cpBeige">
-      <h1 className="font-qtBasker text-4xl text-cpText lg:text-9xl">{blok?.title}</h1>
-      <h2>{blok?.subheading}</h2>
+    <header
+      {...storyblokEditable(blok)}
+      key={blok?._uid}
+      id={blok?._uid}
+      className="relative h-[113vh] w-full"
+      style={{ background: 'url(/assets/bg-header.svg) no-repeat' }}>
+      <div className="flex h-3/4 w-full flex-col items-center justify-center">
+        <h1 className="select-none py-4 font-qtBasker text-5xl text-cpText md:text-6xl lg:text-9xl">{blok?.title}</h1>
+        <h2 className="select-none py-4 font-poppins text-2xl font-light md:text-4xl lg:text-5xl">{blok?.subheading}</h2>
+      </div>
     </header>
   );
 };
