4c04436488b1bf92c09008d3efb229cbfcff1042
schmaenjael <mayer.manuel@protonmail.ch>
2024-01-28 17:27:15 +0100

    feat(ordination): add ordination section

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -6,7 +6,7 @@ export const Header = ({ blok }: any) => {
       {...storyblokEditable(blok)}
       key={blok?._uid}
       id={blok?._uid}
-      className="relative h-[113vh] w-full"
+      className="relative h-[120vh] w-full"
       style={{ background: 'url(/assets/bg-header.svg) no-repeat' }}>
       <div className="flex h-3/4 w-full flex-col items-center justify-center">
         <h1 className="select-none py-4 text-center font-qtBasker text-5xl text-cpText md:text-6xl lg:text-9xl">{blok?.title}</h1>
--- src/components/sections/Ordination.tsx
+++ src/components/sections/Ordination.tsx
@@ -2,8 +2,28 @@ import { storyblokEditable } from '@storyblok/react';
 
 export const Ordination = ({ blok }: any) => {
   return (
-    <div {...storyblokEditable(blok)} key={blok?._uid} id={blok?._uid} className="relative h-screen w-full">
-      <h1>Ordination</h1>
-    </div>
+    <>
+      <img src="/assets/wave-4.svg" className="relative -bottom-1 z-10 w-full" />
+      <div
+        {...storyblokEditable(blok)}
+        key={blok?._uid}
+        id={blok?._uid}
+        className="relative flex h-fit min-h-screen w-full flex-col items-center justify-evenly bg-cpBeige py-4">
+        <h3 className="text-center font-poppins text-2xl font-bold text-cpText md:text-4xl lg:text-5xl">{blok.title}</h3>
+        <div className="flex h-2/3 min-h-60 w-3/4 flex-col items-center justify-evenly rounded-2xl bg-white">
+          {blok?.ordination_content.map(({ title, icon, text, _uid }: { [key: string]: any }) => (
+            <div className="flex h-fit flex-col items-center justify-evenly px-12 py-8 [&>*]:my-2" key={_uid}>
+              <div className="aspect-square w-16  rounded-2xl bg-cpRed bg-opacity-30 p-3">
+                <img src={icon.filename} className="aspect-square w-full" />
+              </div>
+              <h4 className="text-center font-poppins text-xl font-semibold text-cpText">{title}</h4>
+              <p className="w-full whitespace-pre-wrap text-center font-mulish text-lg text-cpText [text-align-last:center] md:text-justify">
+                {text}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
